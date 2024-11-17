af25d6e77633fd5b445042402f6700c397b05bff
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2024-11-17 11:24:04 +0100

    Added padding method

--- tokenizer.py
+++ tokenizer.py
@@ -1,13 +1,21 @@
 from nltk import word_tokenize, FreqDist
+from sympy import sequence
+
+
+# TODO: Add padding
 
 class Tokenizer:
     def __init__(self, file):
         self.file = file
+        # Build a vocab out of given file directly after initialization
+        self.vocab = self._build_vocab()
 
-    def tokenize(self):
-        """
-        This method returns vocab dictionary, where all tokens are assigned a frequency num
-        """
+    def __len__(self):
+        return len(self.vocab)
+
+    def _build_vocab(self):
+        """Moving tokenize() method functionality here,
+        as this method will build vocabulary, tokenize() will be used for tokenizing captions"""
         with open(self.file, 'r') as f:
             lines = f.readlines()
             just_text = []
@@ -15,7 +23,6 @@ class Tokenizer:
                 just_text.append(line.split('\t')[1])
 
         f.close()
-
         tokenized_sentences = []
 
         for line in just_text:
@@ -29,6 +36,7 @@ class Tokenizer:
         freq_dist = FreqDist(all_tokens)
 
         # Special tokens for padding, start/end of sentence and unknown tokens
+        # 0, 1, 2, 3 indexes for special tokens
         special_tokens = ['<PAD>', '<SOS>', '<EOS>', '<UNK>']
 
         # Start a vocab dict.
@@ -42,3 +50,25 @@ class Tokenizer:
                 vocab[word] = len(vocab)
 
         return vocab
+
+    def tokenize(self, caption:str):
+        """
+        This method returns tokenized caption seq.
+        """
+        tokens = word_tokenize(caption)
+        # Get the token number from dict or unknown
+        token_ind = [self.vocab.get(token, self.vocab['<UNK>']) for token in tokens]
+        # Return a sentence SOS + tokens + EOS
+        return [self.vocab['<SOS>']] + token_ind + [self.vocab['<EOS>']]
+
+    def pad_sequence(self, seq, max_length):
+        """
+        This method pads the tokenized caption seq.
+        """
+        pad_index = self.vocab['<PAD>']
+        if len(seq) < max_length:
+            # SEQ + PAD + PAD + PAD...
+            seq += [pad_index] * (max_length - len(seq))
+        else:
+            seq = seq[:max_length]
+        return seq
